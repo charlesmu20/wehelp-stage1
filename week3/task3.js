216989da54664561ba0d69f397e83f16d17a23f6
Charles Mu <charlesmoh2000@gmail.com>
2026-06-25 22:22:45 +0800

    Task4

--- week3/task3.js
+++ week3/task3.js
@@ -1,21 +1,21 @@
 function toggle() {
     document.getElementById('burgerMenu').classList.toggle('active');
 };
+let currentIndex = 13;
+let spots=[];
+const ContentBlocks = document.querySelector(".Content-blocks");
 async function init() {
     const res1 = await fetch("https://cwpeng.github.io/test/assignment-3-1");
     const data1 = await res1.json();
     
     const res2 = await fetch("https://cwpeng.github.io/test/assignment-3-2");
     const data2 = await res2.json();
-    rows2 = data2.rows
-    console.log(data2.host)
     //用serial建立圖片的物件
     const picMap={};
-    for (const item of rows2){
+    for (const item of data2.rows){
         picMap[item.serial] = item.pics;
     }
-    //建立spots
-    const spots= [];
+    //資料放進spots
     for (const item of data1.rows){
         const pics = picMap[item.serial];
         let firstPic="";
@@ -42,7 +42,6 @@ async function init() {
         barsDiv.appendChild(bar);
     }
     //渲染Content blocks
-    const ContentBlocks = document.querySelector(".Content-blocks")
     for (let i=3 ; i<13 ;i++){
         //block
         const block = document.createElement("div");
@@ -57,21 +56,42 @@ async function init() {
         span.textContent="⭐";
         //文字div
         const blockText = document.createElement("div");
-        blockText.classList = "block-text";
+        blockText.className = "block-text";
         blockText.textContent = spots[i].name;
         
         block.appendChild(span);
         block.appendChild(blockText);
         ContentBlocks.appendChild(block);
     }
-    let currentIndex = 13
-    function loadMore(){
-        for (let i=currentIndex;i<currentIndex+10;i++){
-
-        }
-        currentIndex+=10
-    }
 };
+//loadMore
+function loadMore(){
+        const end = Math.min(currentIndex+10,spots.length);
 
+    for (let i=currentIndex;i<end;i++){
+        const block = document.createElement("div");
+        block.className = "block";
+        block.style.backgroundImage = `url(${spots[i].image})`
+        //星星span
+        const span = document.createElement("span");
+        span.className = "star";
+        span.textContent="⭐";
+        //文字div
+        const blockText = document.createElement("div");
+        blockText.className = "block-text";
+        blockText.textContent = spots[i].name;
+        
+        block.appendChild(span);
+        block.appendChild(blockText);
+        ContentBlocks.appendChild(block); 
+    };
+    currentIndex+=10;
+    //最後跑完隱藏按鈕
+    if (currentIndex >= spots.length){
+        loadBtn.style.display = "none";
+    };
+}
+const loadBtn = document.querySelector(".load");
+loadBtn.addEventListener('click',loadMore);
 init()
 
